a7437ed59df26392f5acbd4115cb25a8cee64277
Web Developer <useraccount@WebDeveloperMBP.local>
2022-05-16 10:43:00 -0500

    Version Update

--- react/GlossaryNav.tsx
+++ react/GlossaryNav.tsx
@@ -15,9 +15,63 @@ const GlossaryNav: StorefrontFunctionComponent<GlossaryNavProps> = ({ }) => {
     console.clear();
   })
 
+  const goToLetter = (e: any) => {
+    const letter = e.target.innerText.toLowerCase();
+
+    disappearHeadroom();
+
+    //@ts-expect-error
+    document.location.href = "#letter-" + ((letter !== "#") ? letter : "numeral");
+    console.log("Woof")
+  }
+
+  const disappearHeadroom = () => {
+    // Prevents the Head Room app from covering the scrolled to content - LM
+
+    //@ts-expect-error
+    const headRoom: any = document.getElementsByClassName("headroom")[0];
+    headRoom.style.position = "static";
+  }
+
   return (
     <nav>
-      <h1>Hello World!</h1>
+      <div className={styles.glossaryContainer}>
+        <div className={styles.glossaryColumnOne}>
+          <div className={styles.colOneContainer}>
+            <div className={styles.letter} onClick={goToLetter}>#</div>
+            <div className={styles.letter} onClick={goToLetter}>A</div>
+            <div className={styles.letter} onClick={goToLetter}>B</div>
+            <div className={styles.letter} onClick={goToLetter}>C</div>
+            <div className={styles.letter} onClick={goToLetter}>D</div>
+            <div className={styles.letter} onClick={goToLetter}>E</div>
+            <div className={styles.letter} onClick={goToLetter}>F</div>
+            <div className={styles.letter} onClick={goToLetter}>G</div>
+            <div className={styles.letter} onClick={goToLetter}>H</div>
+            <div className={styles.letter} onClick={goToLetter}>I</div>
+            <div className={styles.letter} onClick={goToLetter}>J</div>
+            <div className={styles.letter} onClick={goToLetter}>K</div>
+            <div className={styles.letter} onClick={goToLetter}>L</div>
+            <div className={styles.letter} onClick={goToLetter}>M</div>
+          </div>
+        </div>
+        <div className={styles.glossaryColumnTwo}>
+          <div className={styles.colTwoContainer}>
+            <div className={styles.letter} onClick={goToLetter}>N</div>
+            <div className={styles.letter} onClick={goToLetter}>O</div>
+            <div className={styles.letter} onClick={goToLetter}>P</div>
+            <div className={styles.letter} onClick={goToLetter}>Q</div>
+            <div className={styles.letter} onClick={goToLetter}>R</div>
+            <div className={styles.letter} onClick={goToLetter}>S</div>
+            <div className={styles.letter} onClick={goToLetter}>T</div>
+            <div className={styles.letter} onClick={goToLetter}>U</div>
+            <div className={styles.letter} onClick={goToLetter}>V</div>
+            <div className={styles.letter} onClick={goToLetter}>W</div>
+            <div className={styles.letter} onClick={goToLetter}>X</div>
+            <div className={styles.letter} onClick={goToLetter}>Y</div>
+            <div className={styles.letter} onClick={goToLetter}>Z</div>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
